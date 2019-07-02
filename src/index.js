9b9e3b34ccf03325a61ff84a662794a9aef77b98
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-07-02 15:10:41 +0100

    changed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  ivMc = document.querySelector('div')
+  formEl = document.querySelector('#create-task-form')
+})
+
+
+ const formEl = document.querySelector('#create-task-form')
+ const descEl = document.querySelector('#new-task-description')
+
+ const tasks = document.querySelector('#tasks')
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const taskLi = document.createElement('li') // result: <li></li>
+
+  taskLi.innerText = `${ descEl.value}`
+
+  tasks.append(taskLi)
+// descEl.value = ''
+  formEl.reset()
+})
+ 
+
+// append apple to tree
